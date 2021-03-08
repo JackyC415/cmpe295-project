3f0ef75569de66b7a86bd3f7beebef4baa5455b7
Huy Huynh <huyhuynh@Huys-MacBook-Pro.local>
2021-03-08 00:07:31 -0800

    fix dbparser.js to parse resumes-data.csv and jobs-data.csv

--- server/dbparser.js
+++ server/dbparser.js
@@ -10,23 +10,39 @@ const { Double } = require("mongodb");
 const dir = '../APIFY-Crawler-Server/files/';
 
 const Json2csvParser = require("json2csv").Parser;
+var fileObjs=init();
+var allStr=["id,description"];//contains all jobs describtion in string format
+var countID=0;
 
 function init() {
-  return fs.readdirSync(dir)
+  var returnObj= fs.readdirSync(dir)
            .filter(name => path.extname(name) === '.json')
            .map(name => require(path.join(dir, name)));
+  
+  return returnObj;
 }
-var fileObjs=init();
+//Write arrays of data to a file at path
+function writeArrToFile( path, data)
+{
+  fs.writeFile(path, data.join("\r\n"), (err) => {
+    console.log(err || "Wrote to "+path);  });
 
+}
 fileObjs.forEach(file => { 
-    console.log(file); 
+    var str=JSON.stringify(file).replace(/[\",\{,\}]/g, '');
+    countID++;
+    allStr.push(countID+",\""+str+'\"');
 });
+writeArrToFile("../jobs-data.csv",allStr);
 
 //Create jobs collection
 var MongoClient = require('mongodb').MongoClient;
+const { AsyncLocalStorage } = require("async_hooks");
+const { forEach } = require("jszip");
+const { nextTick } = require("process");
 var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) 
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) 
 {
   if (err) throw err;
   var dbo = db.db("cmpe295");
@@ -43,25 +59,24 @@ MongoClient.connect(url, function(err, db)
   dbo.collection("jobs").insertMany(fileObjs, function(err, res) {
     if (err) throw err;
     console.log("Number of documents inserted: " + res.insertedCount);
+    //Write to resume-data.csv
+  var resumeData=["id,description"];
+
+  //Write to resumes-data.csv
+  dbo.collection("parsers").distinct("contents")
+    .then(resume => {
+      resumeData.push(1+",\""+resume+"\"");//Todo: change 1 to id when there are more than 1 resume
+      writeArrToFile("../resumes-data.csv",resumeData);
+      db.close();
+    });
+    
   });
-  var resumeData="id,description\n";
-
-  //Write resume to CSV
-  dbo.collection("parsers").find({}).toArray((err, data) => 
-  {
-    if (err) throw err;
-
-    console.log(data);
-    const json2csvParser = new Json2csvParser({ header: false });
-    resumeData += json2csvParser.parse(data);
-  });
-
+  
+/*
   //Write resume+jobs to CSV
   dbo.collection("jobs").find({}).toArray((err, data) => 
   {
     if (err) throw err;
-
-    console.log(data);
     const json2csvParser = new Json2csvParser({ header: false });
     const jobsData = json2csvParser.parse(data);
     const csvData=resumeData+jobsData;
@@ -69,7 +84,6 @@ MongoClient.connect(url, function(err, db)
       if (error) throw error;
       console.log("Write to jobs-data.csv successfully!");
     })
-    db.close();
-  });
-  
+    
+  });*/
 });
