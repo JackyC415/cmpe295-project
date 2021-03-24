f542138484c1224f5ad8b5a65a3211a6ac7efd8f
Huy Huynh <huyhuynh@Huys-MacBook-Pro.local>
2021-03-23 23:10:00 -0700

    Added Skills Compare Column

--- server/dbparser.js
+++ server/dbparser.js
@@ -7,7 +7,9 @@ const path = require('path');
 const { json } = require("body-parser");
 const { Double } = require("mongodb");
 
-const dir = '../APIFY-Crawler-Server/files/';
+const dir = '../APIFY-Crawler-Server/file2/';
+const jobsDataFileLoc="../jobs-data.csv";
+const skillCollectionFileLoc="../tech_skills.csv";
 
 const Json2csvParser = require("json2csv").Parser;
 var fileObjs=init();
@@ -28,13 +30,16 @@ function writeArrToFile( path, data)
     console.log(err || "Wrote to "+path);  });
 
 }
+/*
+//Write jobs data to csv
 fileObjs.forEach(file => { 
-    var str=JSON.stringify(file).replace(/[\",\{,\}]/g, '');
+    var str=JSON.stringify(file).replace(/\"/g, '');
+    str=JSON.stringify(file).replace(/"{}"/g, ',');
     countID++;
     allStr.push(countID+",\""+str+'\"');
 });
 writeArrToFile("../jobs-data.csv",allStr);
-
+*/
 //Create jobs collection
 var MongoClient = require('mongodb').MongoClient;
 const { AsyncLocalStorage } = require("async_hooks");
@@ -46,22 +51,30 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, fu
 {
   if (err) throw err;
   var dbo = db.db("cmpe295");
-  
-  dbo.createCollection("jobs", function(err, res) {
-    if (err) 
-        console.log(err);
-    else
-        console.log("Collection created!");
-    
-  });
+  dbo.listCollections({name: "jobs"})
+    .next(function(err, collinfo) {
+        if (collinfo) {
+          console.log("jobs collection already in db");
+        }
+        else{
+          dbo.createCollection("jobs", function(err, res) {
+            if (err) 
+                console.log(err);
+            else
+                console.log("Collection created!");
+            
+          });
+        }
+    });
+
   dbo.collection("jobs").deleteMany({});//for now delete first before update new jobs
   //Insert scraped data to jobs collection 
   dbo.collection("jobs").insertMany(fileObjs, function(err, res) {
     if (err) throw err;
     console.log("Number of documents inserted: " + res.insertedCount);
-    //Write to resume-data.csv
+  
+  /*
   var resumeData=["id,description"];
-
   //Write to resumes-data.csv
   dbo.collection("parsers").distinct("contents")
     .then(resume => {
@@ -69,21 +82,21 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, fu
       writeArrToFile("../resumes-data.csv",resumeData);
       db.close();
     });
-    
+   */ 
   });
   
-/*
-  //Write resume+jobs to CSV
+
+  //Write jobs to CSV
   dbo.collection("jobs").find({}).toArray((err, data) => 
   {
     if (err) throw err;
-    const json2csvParser = new Json2csvParser({ header: false });
+    const json2csvParser = new Json2csvParser({ header: true });
     const jobsData = json2csvParser.parse(data);
-    const csvData=resumeData+jobsData;
-    fs.writeFile("../jobs-data.csv", csvData, function(error) {
+    const csvData=jobsData;
+    fs.writeFile(jobsDataFileLoc, csvData, function(error) {
       if (error) throw error;
       console.log("Write to jobs-data.csv successfully!");
     })
-    
-  });*/
+    db.close();
+  });
 });
---
+++ server/skillTechProcess.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const { forEach } = require('jszip');
+const { stringify } = require('querystring');
+const Json2csvParser = require("json2csv").Parser;
+const jobsDataFileLoc="../jobs-data.csv";
+const jobsSkillDataFileLoc="../jobs-skills-data.csv";
+const skillCollectionFileLoc="../tech_skills.csv";
+
+
+var jobData = fs.readFileSync(jobsDataFileLoc)
+    .toString() // convert Buffer to string
+    .split('\n') // split string to lines
+    .map(e => e.trim()); // remove white spaces for each line
+
+var skillDictionary=fs.readFileSync(skillCollectionFileLoc)
+.toString() // convert Buffer to string
+.split('\n') // split string to lines
+.map(e => e.trim()); // remove white spaces for each line
+
+//var skillMatchArr=["Skills"];
+//jobData.forEach(eachJob => {
+jobData[0]+=",Skills";
+for (var jobIndex=1;jobIndex<jobData.length;jobIndex++)
+{
+    var eachJob=jobData[jobIndex];
+    var skillStr="";
+    skillDictionary.forEach( skill =>{
+        if (eachJob.includes(skill))
+        {
+            skillStr+=skill+" ";
+        }
+    });
+    eachJob+=", "+skillStr;
+    jobData[jobIndex]=eachJob;
+};
+
+
+const os = require('os');
+fs.writeFileSync(jobsSkillDataFileLoc, jobData.join(os.EOL));
