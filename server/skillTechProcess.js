f860cd4722232b0f12a019b92be0c2c9dea8bc55
Huy Huynh <huyhuynh1228@gmail.com>
2021-03-24 22:00:54 -0700

    improve skills recognition

--- server/skillTechProcess.js
+++ server/skillTechProcess.js
@@ -4,10 +4,17 @@ const { stringify } = require('querystring');
 const Json2csvParser = require("json2csv").Parser;
 const jobsDataFileLoc="jobs-data.csv";
 const jobsSkillDataFileLoc="jobs-skills-data.csv";
+const resumeFileLoc="resumeData.csv";
+const resumeSkillFileLoc="resume-skill.csv";
 const skillCollectionFileLoc="tech_skills.csv";
 
 
 var jobData = fs.readFileSync(jobsDataFileLoc)
+    .toString() // convert Buffer to string
+    .split('\n') // split string to lines
+    .map(e => e.trim()); // remove white spaces for each line
+
+var resumeData = fs.readFileSync(resumeFileLoc)
     .toString() // convert Buffer to string
     .split('\n') // split string to lines
     .map(e => e.trim()); // remove white spaces for each line
@@ -17,26 +24,31 @@ var skillDictionary=fs.readFileSync(skillCollectionFileLoc)
 .split('\n') // split string to lines
 .map(e => e.trim()); // remove white spaces for each line
 
-//var skillMatchArr=["Skills"];
-//jobData.forEach(eachJob => {
-jobData[0]+=",Skills";
-for (var jobIndex=1;jobIndex<jobData.length;jobIndex++)
+
+function addSkillsAndRewrite(dataArr,fileLoc)
 {
-    var eachJob=jobData[jobIndex];
-    var skillStr="";
-    skillDictionary.forEach( skill =>{
-        if (eachJob.includes(skill))
-        {
-            skillStr+=skill+" ";
+    //Add skill data for jobsData
+    dataArr[0]+=",Skills";
+    for (var jobIndex=1;jobIndex<dataArr.length;jobIndex++)
+    {
+        var eachJob=dataArr[jobIndex].toLowerCase();
+        var skillStr="";
+        skillDictionary.forEach( skill =>{
+            if (eachJob.includes(skill.toLowerCase()))
+            {
+                skillStr+=skill.toLowerCase()+" ";
+            }
+        });
+        skillStr=skillStr.trim();
+        if (skillStr!=""){
+            eachJob+=", "+skillStr;
         }
-    });
-    skillStr=skillStr.trim();
-    if (skillStr!=""){
-        eachJob+=", "+skillStr;
-    }
-    jobData[jobIndex]=eachJob;
-};
+        dataArr[jobIndex]=eachJob;
+    };
 
-
-const os = require('os');
-fs.writeFileSync(jobsSkillDataFileLoc, jobData.join(os.EOL));
+    //write back to job-skill-data.csv
+    const os = require('os');
+    fs.writeFileSync(fileLoc, dataArr.join(os.EOL));
+}
+addSkillsAndRewrite(jobData,jobsSkillDataFileLoc);
+addSkillsAndRewrite(resumeData,resumeSkillFileLoc);
