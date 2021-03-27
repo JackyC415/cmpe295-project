8d90d51ea4c33ae4c93e674582865a980864a299
Jacky <jacky.chen@bluescape.com>
2021-03-26 22:02:17 -0700

    update TFIDF and add ratings csv

--- server/TF-IDF.py
+++ server/TF-IDF.py
@@ -15,16 +15,22 @@ tfidf_vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0
 jobs_matrix = tfidf_vectorizer.fit_transform(jobsFile['Skills'])
 resumes_matrix = tfidf_vectorizer.transform(resumesFile['Skills'])
 
+tfidf_vectorizer2 = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words=stopset)
+new_job_matrix = tfidf_vectorizer2.fit_transform(jobsFile['description'])
+new_resumes_matrix = tfidf_vectorizer2.transform(resumesFile['Skills'])
+
 matchingJobsList = []
 matchingRate = 0
 for idx in range(len(jobsFile)):
 	similarScore = cosine_similarity(jobs_matrix[idx],resumes_matrix[0])
-	if similarScore > matchingRate:
+	similarScore2 = cosine_similarity(new_job_matrix[idx],new_resumes_matrix[0])
+
+	if (similarScore+similarScore2) > matchingRate:
 		matchingJobsList.append(
 			{
 			"id": idx, 
 			"title": jobsFile['title'][idx], 
-			"score": similarScore[0][0], 
+			"score": similarScore[0][0] + similarScore2[0][0], 
 			"url": jobsFile['url'][idx],
 			"source": jobsFile['source'][idx]
 			})
