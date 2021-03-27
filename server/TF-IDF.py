fb812b352b5f055ab8d3ca63d6025163e7e17bc5
Jacky <jacky.chen@bluescape.com>
2021-03-26 22:28:37 -0700

    update gitinore and renae TFIDF implementation

--- server/TF-IDF.py
+++ server/TF-IDF.py
@@ -7,30 +7,31 @@ import nltk
 from nltk.corpus import stopwords 
 nltk.download('stopwords')
 
-jobsFile = pd.read_csv("jobs-skills-data.csv")
-resumesFile = pd.read_csv("resume-skill.csv")
+jobsFile = pd.read_csv("jobs-data.csv")
+resumesFile = pd.read_csv("resumes-data.csv")
 stopset = set(stopwords.words('english'))
 
 tfidf_vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words=stopset)
-jobs_matrix = tfidf_vectorizer.fit_transform(jobsFile['Skills'])
-resumes_matrix = tfidf_vectorizer.transform(resumesFile['Skills'])
+job_skills_matrix = tfidf_vectorizer.fit_transform(jobsFile['Skills'])
+resume_skills_matrix = tfidf_vectorizer.transform(resumesFile['Skills'])
 
 tfidf_vectorizer2 = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words=stopset)
-new_job_matrix = tfidf_vectorizer2.fit_transform(jobsFile['description'])
-new_resumes_matrix = tfidf_vectorizer2.transform(resumesFile['Skills'])
+job_description_matrix = tfidf_vectorizer2.fit_transform(jobsFile['description'])
+resume_description_matrix = tfidf_vectorizer2.transform(resumesFile['Skills'])
 
 matchingJobsList = []
-matchingRate = 0
+matchingRate = 0.0
 for idx in range(len(jobsFile)):
-	similarScore = cosine_similarity(jobs_matrix[idx],resumes_matrix[0])
-	similarScore2 = cosine_similarity(new_job_matrix[idx],new_resumes_matrix[0])
+	skills_similarity_score = cosine_similarity(job_skills_matrix[idx],resume_skills_matrix[0])
+	description_similarity_score = cosine_similarity(job_description_matrix[idx],resume_description_matrix[0])
+	total_similarity_score = skills_similarity_score + description_similarity_score
 
-	if (similarScore+similarScore2) > matchingRate:
+	if total_similarity_score > matchingRate:
 		matchingJobsList.append(
 			{
 			"id": idx, 
 			"title": jobsFile['title'][idx], 
-			"score": similarScore[0][0] + similarScore2[0][0], 
+			"score": skills_similarity_score[0][0] + description_similarity_score[0][0], 
 			"url": jobsFile['url'][idx],
 			"source": jobsFile['source'][idx]
 			})
