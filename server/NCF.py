e0c7eea8b2f31e2f15831f182b85e4f417817c65
Jacky <jacky.chen@bluescape.com>
2021-03-29 17:56:40 -0700

    add reco-utils module and update NCF implementation

--- server/NCF.py
+++ server/NCF.py
@@ -1,234 +1,73 @@
-import tensorflow.keras as keras
-from tensorflow.keras.layers import (
-    Concatenate,
-    Dense,
-    Embedding,
-    Flatten,
-    Input,
-    Multiply,
-)
-from tensorflow.keras.models import Model
-from tensorflow.keras.regularizers import l2
-from typing import List
-import datetime
+import sys
+sys.path.append("../../")
 import os
-import lightfm
-import numpy as np
+import shutil
+import papermill as pm
+import scrapbook as sb
 import pandas as pd
+import numpy as np
 import tensorflow as tf
-from lightfm import LightFM
-from lightfm.datasets import fetch_movielens
-from lightfm.evaluation import precision_at_k
-from scipy import sparse
-from tensorboard import notebook
-from tensorflow.keras.optimizers import Adam
+import json
+tf.get_logger().setLevel('ERROR') # only show error messages
 
-TOP_K = 5
-N_EPOCHS = 10
+from reco_utils.common.timer import Timer
+from reco_utils.recommender.ncf.ncf_singlenode import NCF
+from reco_utils.recommender.ncf.dataset import Dataset as NCFDataset
+from reco_utils.dataset import movielens
+from reco_utils.dataset.python_splitters import python_chrono_split
+from reco_utils.evaluation.python_evaluation import (rmse, mae, rsquared, exp_var, map_at_k, ndcg_at_k, precision_at_k, 
+                                                     recall_at_k, get_top_k_items)
+from reco_utils.common.constants import SEED as DEFAULT_SEED
 
-data = fetch_movielens(min_rating=3.0)
+TOP_K = 10
 
-print("Interaction matrix:")
-print(data["train"].toarray()[:10, :10])
+# Select MovieLens data size: 100k, 1m, 10m, or 20m
+MOVIELENS_DATA_SIZE = '10k'
 
-for dataset in ["test", "train"]:
-    data[dataset] = (data[dataset].toarray() > 0).astype("int8")
+# Model parameters
+EPOCHS = 100
+BATCH_SIZE = 256
 
-# Make the ratings binary
-print("Interaction matrix:")
-print(data["train"][:10, :10])
+SEED = DEFAULT_SEED  # Set None for non-deterministic results
 
-print("\nRatings:")
-unique_ratings = np.unique(data["train"])
-print(unique_ratings)
+# df = movielens.load_pandas_df(
+#     size=MOVIELENS_DATA_SIZE,
+#     header=["userID", "itemID", "rating", "timestamp"]
+# )
 
+df = pd.read_csv('ratings.csv')
 
-def wide_to_long(wide: np.array, possible_ratings: List[int]) -> np.array:
-    """Go from wide table to long.
-    :param wide: wide array with user-item interactions
-    :param possible_ratings: list of possible ratings that we may have."""
+print(df.head(n=10))
 
-    def _get_ratings(arr: np.array, rating: int) -> np.array:
-        """Generate long array for the rating provided
-        :param arr: wide array with user-item interactions
-        :param rating: the rating that we are interested"""
-        idx = np.where(arr == rating)
-        return np.vstack(
-            (idx[0], idx[1], np.ones(idx[0].size, dtype="int8") * rating)
-        ).T
+train, test = python_chrono_split(df, 0.75)
+data = NCFDataset(train=train, test=test, seed=SEED)
 
-    long_arrays = []
-    for r in possible_ratings:
-        long_arrays.append(_get_ratings(wide, r))
-
-    return np.vstack(long_arrays)
-
-
-def create_ncf(
-    number_of_users: int,
-    number_of_items: int,
-    latent_dim_mf: int = 4,
-    latent_dim_mlp: int = 32,
-    reg_mf: int = 0,
-    reg_mlp: int = 0.01,
-    dense_layers: List[int] = [8, 4],
-    reg_layers: List[int] = [0.01, 0.01],
-    activation_dense: str = "relu",
-) -> keras.Model:
-
-    # input layer
-    user = Input(shape=(), dtype="int32", name="user_id")
-    item = Input(shape=(), dtype="int32", name="item_id")
-
-    # embedding layers
-    mf_user_embedding = Embedding(
-        input_dim=number_of_users,
-        output_dim=latent_dim_mf,
-        name="mf_user_embedding",
-        embeddings_initializer="RandomNormal",
-        embeddings_regularizer=l2(reg_mf),
-        input_length=1,
-    )
-    mf_item_embedding = Embedding(
-        input_dim=number_of_items,
-        output_dim=latent_dim_mf,
-        name="mf_item_embedding",
-        embeddings_initializer="RandomNormal",
-        embeddings_regularizer=l2(reg_mf),
-        input_length=1,
-    )
-
-    mlp_user_embedding = Embedding(
-        input_dim=number_of_users,
-        output_dim=latent_dim_mlp,
-        name="mlp_user_embedding",
-        embeddings_initializer="RandomNormal",
-        embeddings_regularizer=l2(reg_mlp),
-        input_length=1,
-    )
-    mlp_item_embedding = Embedding(
-        input_dim=number_of_items,
-        output_dim=latent_dim_mlp,
-        name="mlp_item_embedding",
-        embeddings_initializer="RandomNormal",
-        embeddings_regularizer=l2(reg_mlp),
-        input_length=1,
-    )
-
-    # MF vector
-    mf_user_latent = Flatten()(mf_user_embedding(user))
-    mf_item_latent = Flatten()(mf_item_embedding(item))
-    mf_cat_latent = Multiply()([mf_user_latent, mf_item_latent])
-
-    # MLP vector
-    mlp_user_latent = Flatten()(mlp_user_embedding(user))
-    mlp_item_latent = Flatten()(mlp_item_embedding(item))
-    mlp_cat_latent = Concatenate()([mlp_user_latent, mlp_item_latent])
-
-    mlp_vector = mlp_cat_latent
-
-    # build dense layers for model
-    for i in range(len(dense_layers)):
-        layer = Dense(
-            dense_layers[i],
-            activity_regularizer=l2(reg_layers[i]),
-            activation=activation_dense,
-            name="layer%d" % i,
-        )
-        mlp_vector = layer(mlp_vector)
-
-    predict_layer = Concatenate()([mf_cat_latent, mlp_vector])
-
-    result = Dense(
-        1, activation="sigmoid", kernel_initializer="lecun_uniform", name="interaction"
-    )
-
-    output = result(predict_layer)
-
-    model = Model(
-        inputs=[user, item],
-        outputs=[output],
-    )
-
-    return model
-
-def make_tf_dataset(
-    df: pd.DataFrame,
-    targets: List[str],
-    val_split: float = 0.1,
-    batch_size: int = 512,
-    seed=42,
-):
-    """Make TensorFlow dataset from Pandas DataFrame.
-    :param df: input DataFrame - only contains features and target(s)
-    :param targets: list of columns names corresponding to targets
-    :param val_split: fraction of the data that should be used for validation
-    :param batch_size: batch size for training
-    :param seed: random seed for shuffling the data - setting to `None` will not shuffle the data"""
-
-    n_val = round(df.shape[0] * val_split)
-    if seed:
-        # shuffle all the rows
-        x = df.sample(frac=1, random_state=seed).to_dict("series")
-    else:
-        x = df.to_dict("series")
-    y = dict()
-    for t in targets:
-        y[t] = x.pop(t)
-    ds = tf.data.Dataset.from_tensor_slices((x, y))
-
-    ds_val = ds.take(n_val).batch(batch_size)
-    ds_train = ds.skip(n_val).batch(batch_size)
-    return ds_train, ds_val
-
-
-long_train = wide_to_long(data["train"], unique_ratings)
-df_train = pd.DataFrame(long_train, columns=["user_id", "item_id", "interaction"])
-print("All interactions:")
-df_train.head()
-print("Only positive interactions:")
-df_train[df_train["interaction"] > 0].head()
-
-n_users, n_items = data["train"].shape
-ncf_model = create_ncf(n_users, n_items)
-
-ncf_model.compile(
-    optimizer=Adam(),
-    loss="binary_crossentropy",
-    metrics=[
-        tf.keras.metrics.TruePositives(name="tp"),
-        tf.keras.metrics.FalsePositives(name="fp"),
-        tf.keras.metrics.TrueNegatives(name="tn"),
-        tf.keras.metrics.FalseNegatives(name="fn"),
-        tf.keras.metrics.BinaryAccuracy(name="accuracy"),
-        tf.keras.metrics.Precision(name="precision"),
-        tf.keras.metrics.Recall(name="recall"),
-        tf.keras.metrics.AUC(name="auc"),
-    ],
-)
-ncf_model._name = "neural_collaborative_filtering"
-ncf_model.summary()
-
-ds_train, ds_val = make_tf_dataset(df_train, ["interaction"])
-logdir = os.path.join("logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
-early_stopping_callback = tf.keras.callbacks.EarlyStopping(
-    monitor="val_loss", patience=0
+model = NCF (
+    n_users=data.n_users, 
+    n_items=data.n_items,
+    model_type="NeuMF",
+    n_factors=4,
+    layer_sizes=[16,8,4],
+    n_epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
+    learning_rate=1e-3,
+    verbose=10,
+    seed=SEED
 )
 
-train_hist = ncf_model.fit(
-    ds_train,
-    validation_data=ds_val,
-    epochs=N_EPOCHS,
-    callbacks=[tensorboard_callback, early_stopping_callback],
-    verbose=1,
-)
+with Timer() as train_time:
+    model.fit(data)
 
-long_test = wide_to_long(data["train"], unique_ratings)
-df_test = pd.DataFrame(long_test, columns=["user_id", "item_id", "interaction"])
-ds_test, _ = make_tf_dataset(df_test, ["interaction"], val_split=0, seed=None)
+print("Took {} seconds for training.".format(train_time.interval))
 
-ncf_predictions = ncf_model.predict(ds_test)
-df_test["ncf_predictions"] = ncf_predictions
+predictions = [[row.userID, row.itemID, model.predict(row.userID, row.itemID)]
+               for (_, row) in test.iterrows()]
 
-df_test.head()
+
+predictions = pd.DataFrame(predictions, columns=['userID', 'itemID', 'prediction'])
+
+#pass json to UI
+predictionJSON = predictions.to_json(orient='records')
+
+#output first five predictions in CLI
+print(predictions.head())
